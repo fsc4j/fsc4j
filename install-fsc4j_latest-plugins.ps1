699ee0c4aec93146de0229fb128abfbf361b1e0b
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-03-27 18:34:36 +0100

    Release 0.1.1 install scripts

--- install-fsc4j_latest-plugins.ps1
+++ install-fsc4j_latest-plugins.ps1
@@ -8,8 +8,8 @@ if (!((test-path "$eclipseHome/plugins") -and (test-path "$eclipseHome/configura
   exit 1
 }
 
-$productVersion = "0.1.0"
-$jdtCoreVersion = "0_1_0"
+$productVersion = "0.1.1"
+$jdtCoreVersion = "0_1_1"
 $jdtDebugVersion = "0_0_2"
 
 $productName = "FSC4J $productVersion"
