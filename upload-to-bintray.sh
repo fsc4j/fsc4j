0d9aebb79862c06349409c75313df776b5eaeb92
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-02-10 14:46:44 +0100

    Remove some obsolete scripts

--- upload-to-bintray.sh
+++
@@ -1,4 +0,0 @@
-curl -T plugins/org.eclipse.jdt.core_3.24.0.vfsc4j_0_5_0-202102012041.jar -u bart-jacobs:$BINTRAY_API_KEY https://api.bintray.com/content/fsc4j/fsc4j/org.eclipse.jdt.core_fsc4j/0.5.0/plugins/org.eclipse.jdt.core_3.24.0.vfsc4j_0_5_0-202102012041.jar
-curl -T features/org.eclipse.jdt_3.18.600.vfsc4j-0_5_0.jar -u bart-jacobs:$BINTRAY_API_KEY https://api.bintray.com/content/fsc4j/fsc4j/org.eclipse.jdt_fsc4j/0.5.0/features/org.eclipse.jdt_3.18.600.vfsc4j-0_5_0.jar
-curl -T artifacts.jar -u bart-jacobs:$BINTRAY_API_KEY https://api.bintray.com/content/fsc4j/fsc4j/artifacts.jar
-curl -T content.jar -u bart-jacobs:$BINTRAY_API_KEY https://api.bintray.com/content/fsc4j/fsc4j/content.jar
